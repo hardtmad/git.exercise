3262e0e0280222703ef9ee6459f9060412fc192f
Madeleine Hardt <hardtmad@grinnell.edu>
2014-01-23 09:03:54 -0600

    Change on GitHub.

--- src/edu/grinnell/csc207/username/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/username/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
   public static void 
     main (String[] args) 
   {
-    System.out.println ("Hello again, GitHub.");
+    System.out.println ("Hi there, GitHub.");
     System.out.println ("Goodbye again, GitHub!");
   } // main(String[])
 }
