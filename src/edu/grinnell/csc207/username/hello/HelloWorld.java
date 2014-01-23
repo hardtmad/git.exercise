de40d5f14f02ba85755e235e9882726cf186043e
hardtmad <hardtmad@grinnell.edu>
2014-01-23 08:54:31 -0600

    add src file

---
+++ src/edu/grinnell/csc207/username/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.grinnell.csc207.username.hello;
+
+public class HelloWorld
+{
+  public static void 
+    main (String[] args) 
+  {
+    System.out.println ("Hello, GitHub.");
+    System.out.println ("Goodbye, GitHub!");
+  } // main(String[])
+}
