ca26b2f58b9ad52e204c38a3ab60f32377fa932e
Madeleine Hardt <hardtmad@grinnell.edu>
2014-01-23 08:58:24 -0600

    Change to be pulled by local rep.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
   public static void 
     main (String[] args) 
   {
-    System.out.println ("Hello, GitHub.");
+    System.out.println ("Hello again, GitHub.");
     System.out.println ("Goodbye, GitHub!");
   } // main(String[])
 }
